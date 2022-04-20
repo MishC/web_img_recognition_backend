c218559d090633b03d37ba56de1f8196bacf97e7
MishC <michaela.chovancova@gmail.com>
2022-04-20 16:56:48 +0200

    image entries increment

--- controllers/image.js
+++ controllers/image.js
@@ -35,10 +35,10 @@ const handleImage = (req, res, db) => {
   const { id } = req.body;
   db("users")
     .where("id", "=", id)
-    .increment("entries", 1)
+    .increment("entries", +1)
     .returning("entries")
     .then((entries) => {
-      res.json(entries[0].entries);
+      res.json(entries);
     })
     .catch((err) => res.status(400).json("unable to get entries"));
 };
