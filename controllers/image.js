b13f4aad87dac6003fc9ecbffe7192752cb1efa7
MishC <michaela.chovancova@gmail.com>
2022-04-20 15:43:15 +0200

    res.json entries

--- controllers/image.js
+++ controllers/image.js
@@ -37,7 +37,9 @@ const handleImage = (req, res, db) => {
     .where("id", "=", id)
     .increment("entries", 1)
     .returning("entries")
-    .then((entries) => console.log(entries[0].entries))
-    .catch((err) => res.status(400).json("Unable to get entries"));
+    .then((entries) => {
+      res.json(entries[0].entries);
+    })
+    .catch((err) => res.status(400).json("unable to get entries"));
 };
 module.exports = { handleImage, handleApiCall };
