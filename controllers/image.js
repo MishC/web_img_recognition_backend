bd66636c233019f0fe9e7c22477e59d53b11c75f
MishC <michaela.chovancova@gmail.com>
2022-06-03 03:16:47 +0200

    npm audit

--- controllers/image.js
+++ controllers/image.js
@@ -39,7 +39,6 @@ const handleImage = (req, res, db) => {
     .returning("entries")
     .then((entries) => {
       res.json(entries[0].entries);
-      //console.log(entries[0].entries);
     })
     .catch((err) => res.status(400).json("unable to get entries"));
 };
